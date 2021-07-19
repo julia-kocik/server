32db096ac1148a83f0b205d91509c7b13571a5a6
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-07-19 12:27:08 +0200

    Add templates

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-      res.sendFile(path.join(__dirname, `/views/${name}`));
-    };
-    next();
-});
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs')
+
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/user', (req, res) => {
-    res.show('forbidden.html');
+    res.render('forbidden')
 });
 
 app.get('/', (req, res) => {
-    res.show('index.html');
+    res.render('index')
 });
 
 app.get('/home', (req, res) => {
-    res.show('index.html');
+    res.render('index')
 });
 
 app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('about')
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
-    res.status(404).show('404.html');
+    res.status(404).render('404');
 })
 
 app.listen(8000, () => {
